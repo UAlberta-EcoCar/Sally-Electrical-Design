42575ae052261b5dea38b79e9ec9ac2195b45c01
owang-587 <baiwen@ualberta.ca>
2025-01-28 00:41:53 -0500

    finish H2 MoSen layout

--- Electrical-Hardware-Design-H2-Sensor-Board/H2-Sensor-Board2024_Sally/H2-Board-Firmware/Debug/Core/Src/bme280/bme280.d
+++
@@ -1,4 +0,0 @@
-Core/Src/bme280/bme280.o: ../Core/Src/bme280/bme280.c \
- ../Core/Src/bme280/bme280.h ../Core/Src/bme280/bme280_defs.h
-../Core/Src/bme280/bme280.h:
-../Core/Src/bme280/bme280_defs.h:
---
+++ Electrical-Hardware-Design-H2-Sensor-Board/H2-Sensor-Board2024_Sally/H2-Board-Firmware/README.txt
@@ -0,0 +1,2 @@
+refer to H2_Lucy dir for previously written firmware
+keeping this dir clean for all things Sally
