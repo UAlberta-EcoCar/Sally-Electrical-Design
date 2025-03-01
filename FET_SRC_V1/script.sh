f20e4c23f483477de2818a2d75a49a6fca578a41
ddjangl <semmens@ualberta.ca>
2025-02-28 22:41:20 -0700

    $

--- FET_SRC_V1/script.sh
+++ FET_SRC_V1/script.sh
@@ -2,7 +2,7 @@
 
 echo "Configuring project tree for builds..."
 
-rm -rf build/Debug && mkdir build/Debug && cmake -B build/Debug -G Ninja
+rm -rf build/Debug && mkdir build && mkdir build/Debug && cmake -B build/Debug -G Ninja
 
 echo "Call 'make' to build the project..."
 
